91d2a69e1caae6cc7be36685aad44fbee6478a86
fantasyinferno <fantasyinferno@gmail.com>
2017-06-22 15:50:33 +0700

    account fix

--- account_role.sql
+++ account_role.sql
@@ -1,6 +1,6 @@
 drop table if exists account_role;
 create table account_role(account_id INTEGER, role_id INTEGER, PRIMARY KEY(account_id, role_id));
-insert into account_role(account_id, role_id) values (1,1), (1,2), (2, 1), (3,1), (4,1), (5,1),
+insert into account_role(account_id, role_id) values (1,1), (1,2), (1,3), (2, 1), (3,1), (4,1), (5,1),
 (6,1),
 (7,1),
 (8,1),
